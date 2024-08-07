fd04318452b0740131e8ca919a3d0af3fc5ecd31
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-08-07 15:21:24 +0600

    ..

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   // Configure environment variables
+  runtimeConfig: {
+    apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080'
+    }
+  },
   ssr:false,
   app: {
     head: {
