1cda04b9a41ee7679efbe2cbaff1d79ca7cbb55b
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-08-12 17:02:30 +0600

    pm2 script update

--- pm2.sh
+++ pm2.sh
@@ -2,9 +2,9 @@
 git pull
 cd /root/crawl-manager/backend
 go build -o crawl-manager-backend
-pm2 restart crawl-manager-backend
+pm2 restart crawl-manager-backend --update-env
 
 cd /root/crawl-manager/frontend
 npm install
 npm run build
-pm2 restart crawl-manager-frontend
+pm2 restart crawl-manager-frontend --update-env
